d025d5ba52afc97884179380af6efefa07cc6e1b
RadiantF-pondok <71006921+RadiantF-pondok@users.noreply.github.com>
2020-09-12 06:49:54 +0700

    menambah 2 files
    aaaa

---
+++ script.js
@@ -0,0 +1,18 @@
+var listDesa = ('argodadi,sedayu',
+    'argomulyo,sedayu',
+    'argorejo,sedayu',
+    'argosari',
+    'bangunjiwo,kasihan',
+    'banguntapan,banguntapan',
+    'bantul,bantul',
+    'baturetno,banguntapan',
+    'bawuran,pleret',
+    'canden,jetis',
+    'caturharjo,pandak',
+    'cetan srigandi'
+);
+document.write('<ol>')
+for (i = 0; i < listDesa.length; i++) {
+    document.write("<li>", listDesa[i], "</li>")
+}
+document.write("</ol>")
